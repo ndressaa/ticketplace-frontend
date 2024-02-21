cab084672c5f376371f84fa68e4607c299d5ad7d
Andressa <andressajucoski@gmail.com>
2024-02-21 01:07:55 -0300

    page structure

---
+++ src/app/events/page.tsx
@@ -0,0 +1,15 @@
+// nessa página fazer a request p o back para
+// carregar a lista de eventos
+
+import Link from 'next/link';
+
+export default function Page() {
+  return (
+    <main>
+      Página de eventos
+      <br />
+      <br />
+      <Link href="./events/1234">Link exemplo evento 1234</Link>
+    </main>
+  );
+}
