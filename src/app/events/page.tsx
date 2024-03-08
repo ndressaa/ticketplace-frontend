3f1e7dcf1e34dde799c5296985c37bb4934b96f3
Andressa <andressajucoski@gmail.com>
2024-03-08 20:02:31 -0300

    página de eventos

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,21 +1,26 @@
-// nessa página fazer a request p o back para
-// carregar a lista de eventos
+import { getAllEvents } from '@/api';
+import { BottomNavBar, EventSlider, Header, Search } from '@/components';
+import { Container, Content } from './styles';
 
-import { BottomNavBar, Header } from '@/components';
-import Link from 'next/link';
-import { Content } from './styles';
+export default async function Page() {
+  const events = await getAllEvents();
 
-export default function Page() {
   return (
     <>
       <Header isLoginOrSignup={false} />
 
       <Content>
-        Página de eventos
-        <br />
-        <br />
-        <Link href="./events/1234">Link exemplo evento 1234</Link>
-        <br />
+        <Container>
+          <Search />
+
+          <EventSlider title="Gênero" />
+
+          <EventSlider title="Artista" />
+
+          <EventSlider title="TOP 10 Shows" />
+
+          {/* <Link href="./events/1234">Link exemplo evento 1234</Link> */}
+        </Container>
       </Content>
 
       <BottomNavBar currentPage="events" />
--- src/app/events/styles.ts
+++ src/app/events/styles.ts
@@ -2,6 +2,18 @@
 
 import { styled } from 'styled-components';
 
-export const Content = styled.main`
+export const Content = styled.div`
   padding-top: 65px;
+  display: flex;
+  justify-content: center;
+  height: 765px;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  max-width: 340px;
+  display: flex;
+  flex-direction: column;
+  padding-top: 20px;
+  gap: 20px;
 `;
